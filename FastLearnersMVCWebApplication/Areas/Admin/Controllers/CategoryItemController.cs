1e78cc6aa792bb1319d5b9b8b9943dbc1e7dc3e1
Ali Pourgheysari <a.geysar@gmail.com>
2022-07-08 18:14:50 +0430

    new controller and views for categoryItem

---
+++ FastLearnersMVCWebApplication/Areas/Admin/Controllers/CategoryItemController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FastLearnersMVCWebApplication.Data;
+using FastLearnersMVCWebApplication.Entities;
+
+namespace FastLearnersMVCWebApplication.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryItemController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/CategoryItem
+        public async Task<IActionResult> Index(int CategoryId)
+        {
+            List<CategoryItem> List = await (from categoryItem in _context.CategoryItem
+                                      where categoryItem.CategoryId == CategoryId
+                                      select new CategoryItem
+                                      {
+                                          CategoryId = categoryItem.Id,
+                                          Description = categoryItem.Description,
+                                          Id = categoryItem.Id,
+                                          Title = categoryItem.Title,
+                                          MediaTypeId = categoryItem.MediaTypeId,
+                                          DateTimeItemReleased = categoryItem.DateTimeItemReleased
+                                      }).ToListAsync();
+
+            return View(List);
+        }
+
+        // GET: Admin/CategoryItem/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryItem = await _context.CategoryItem
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoryItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryItem);
+        }
+
+        // GET: Admin/CategoryItem/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/CategoryItem/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,CategoryId,MediaTypeId,DateTimeItemReleased")] CategoryItem categoryItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(categoryItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryItem);
+        }
+
+        // GET: Admin/CategoryItem/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryItem = await _context.CategoryItem.FindAsync(id);
+            if (categoryItem == null)
+            {
+                return NotFound();
+            }
+            return View(categoryItem);
+        }
+
+        // POST: Admin/CategoryItem/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,CategoryId,MediaTypeId,DateTimeItemReleased")] CategoryItem categoryItem)
+        {
+            if (id != categoryItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoryItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CategoryItemExists(categoryItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryItem);
+        }
+
+        // GET: Admin/CategoryItem/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var categoryItem = await _context.CategoryItem
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (categoryItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryItem);
+        }
+
+        // POST: Admin/CategoryItem/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var categoryItem = await _context.CategoryItem.FindAsync(id);
+            _context.CategoryItem.Remove(categoryItem);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryItemExists(int id)
+        {
+            return _context.CategoryItem.Any(e => e.Id == id);
+        }
+    }
+}
