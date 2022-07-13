902ef532c5c94d50633feb49be91ccf131be6d9d
Ali Pourgheysari <a.geysar@gmail.com>
2022-07-13 20:02:07 +0430

    Registration dialog

---
+++ FastLearnersMVCWebApplication/Models/RegistrationModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FastLearnersMVCWebApplication.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "User Name")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        [Required]
+        // [RegularExpression(@"\b(?!(\d)\1{3})[13-9]{4}[1346-9][013-9]{5}\b")] //iranian postcode regex
+        [Display(Name = "Post Code")]
+        public string PostCode { get; set; }
+        [Required]
+        [RegularExpression(@"(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}")] //iranian phone number regex
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+        public bool AcceptUserAgreement { get; set; }
+        public string RegistrationInValid { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
