0da35e10d7763d417bd88aaae404edb79a7bc363
Ali Pourgheysari <a.geysar@gmail.com>
2022-07-14 17:32:54 +0430

    UserCategory Functionality
    admin can assign a category to a user or can avoid user to access a category

--- FastLearnersMVCWebApplication/Entities/Category.cs
+++ FastLearnersMVCWebApplication/Entities/Category.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using FastLearnersMVCWebApplication.Interfaces;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FastLearnersMVCWebApplication.Entities
 {
-    public class Category
+    public class Category : IPrimaryProperties
     {
         public int Id { get; set; }
         [StringLength(200, MinimumLength = 3)]
